7618d6aa5860619c2084f8bfdff17186ad85cf47
Shakirat Akanji <akanjishakirato@gmail.com>
2025-03-10 14:00:03 +0100

    Added a timeout to prevent calling the API multiple times while typing

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@ import { createRecipeCard, clearRecipeSection } from "./ui.js";
 
 
 // Bring in data from the html
-const form = document.getElementById("form")
 const input = document.getElementById("search-input")
 const recipeSection = document.getElementById("recipe-cards")
 
@@ -14,30 +13,42 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 })
 
+let debounceTimer;
+
 // Listen for user input in the search field and fetch recipes from the API 
 input.addEventListener("input", function(event){
-  
-  // Collect input data or value
-  const searchInput = input.value.trim()
+  // Clear the previous timer
+  clearTimeout(debounceTimer)
 
-  // Check if user inputs a value and if any meal is found
-  if (searchInput !== ""){
-    // Clear recipes section to remove the random meals
-    clearRecipeSection()
-    fetchSearchMeal(searchInput, function(meals){
-      if (meals.length > 0){
+  // Prevent default form submission
+  event.preventDefault()
+  
+  // Wait before firing API
+  debounceTimer = setTimeout( function() {
+    // Collect input data or value
+    const searchInput = input.value.trim()
+
+    // Check if user inputs a value and if any meal is found
+    if (searchInput !== ""){
+      // Clear recipes section to remove the random meals
+      clearRecipeSection()
+      fetchSearchMeal(searchInput, function(meals){
+        if (meals.length > 0){
         meals.forEach(createRecipeCard)
-      }else {
+        }else {
         recipeSection.innerHTML = "<p>No Recipe Found. Try modifying your search.</>"
-      }
-    })
-  } else {
-    // If input is cleared, reload random meals
-    clearRecipeSection()
-    loadRandomMeals()
-  }
+        }
+     })
+    } else {
+      // If input is cleared, reload random meals
+      clearRecipeSection()
+      loadRandomMeals()
+    }
+  // Delay API call by 0.7s
+  }, 700)
 })
 
+// Fetch and display random meals on clearing input
 function loadRandomMeals() {
   for (let x = 0; x < 18; x++){
     fetchRandomMeal(createRecipeCard)
